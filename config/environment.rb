b4b519382ba30f9cca9a2f7c2a02f0723f9307c9
dreamania <dreamania@gmail.com>
2014-04-02 17:19:40 +0900

    admin: 트래픽 로그 타임존 반

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+module EveHug
+  class Application < Rails::Application
+    # config.force_ssl = false
+    config.time_zone = 'Asia/Seoul'
+  end
+end
