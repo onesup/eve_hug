5e8a492bac6b68669489d2fb2d1b593f94e8a2bd
dreamania <dreamania@gmail.com>
2014-04-11 14:41:38 +0900

    오늘 선정된 이미지 표시, 없으면 최근 선정 이미지 표시

--- app/models/daily_card.rb
+++ app/models/daily_card.rb
@@ -1,4 +1,14 @@
 class DailyCard < ActiveRecord::Base
   mount_uploader :card_image_pc, MessageCardPcUploader
   mount_uploader :card_image_mobile, MessageCardMobileUploader
+  
+  def self.today_image
+    event_day = Time.now.beginning_of_day
+    if self.where(:event_date => event_day).empty?
+      result = self.last
+    else
+      result = self.where(:event_date => event_day)
+    end
+  end
+  
 end
