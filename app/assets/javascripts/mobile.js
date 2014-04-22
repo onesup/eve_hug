6286723f7cf3954f3cb2f6052854b455fadbbe38
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-04-22 18:36:58 +0900

    mobile js 변경

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -12,17 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-// require turbolinks
-//= require phone
-//= require jquery.bpopup.min
-//= require jquery.customSelect.min
-// require icheck.min
-// require jquery.selectBox
-// require jquery.selectric
-// require jquery.maskedinput.min
-//= require jquery.mask
-// jquery.meio.mask.js
-// require jquery.inputmask
 //= require kakao.link.js
 //= require spin
 //= require mobile/mobile.js
