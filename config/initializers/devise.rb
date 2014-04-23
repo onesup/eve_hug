b70acea49f28a315bc6bc52644198d5589825b08
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-04-23 18:39:06 +0900

    log

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -228,7 +228,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-
+  require "omniauth"
   require "omniauth-facebook"
   # OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development? 
   config.omniauth :facebook, 
