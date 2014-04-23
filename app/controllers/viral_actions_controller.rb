d2ec569e7bbef14771f23812b281276f8acda0cf
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-04-24 07:30:04 +0900

    mobile viral_action

--- app/controllers/viral_actions_controller.rb
+++ app/controllers/viral_actions_controller.rb
@@ -1,16 +1,15 @@
 class ViralActionsController < ApplicationController
+  
   def create
-    @viral_action = ViralAction.new(user_params)
+    @viral_action = ViralAction.new(viral_action_params)
+    @viral_action.save
     respond_to do |format|
-      
-      @log = ViralAction.new(device: params[:viral_action][:device], platform: params[:viral_action][:platform], user_id: params[:viral_action][:user_id])
-      @log.save
-    
       format.json { render json: {status: "sucess"}, status: :created}
     end
   end
+  
   private
-    def user_params
+    def viral_action_params
       params.require(:viral_action).permit(:platform, :device, :user_id)
     end
     
