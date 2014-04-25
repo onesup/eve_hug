5c51da6b997a83fc75739f5769829cb01dabcd48
kimpeter <winkim7@gmail.com>
2014-04-25 16:05:18 +0900

    어드민 채널별 유입현황 완료

--- app/controllers/admin/traffic_logs_controller.rb
+++ app/controllers/admin/traffic_logs_controller.rb
@@ -5,36 +5,18 @@ class Admin::TrafficLogsController < ApplicationController
   def index
     
     @traffic_stats = TrafficLog.select("date(convert_tz(created_at,'+00:00','+09:00')) as created_date
-    ,sum(case when source='lgh' then 1 else 0 end) as lgh_count
-    ,sum(case when source='sum37' then 1 else 0 end) as sum37_count
-    ,sum(case when source='lc' then 1 else 0 end) as lc_count
-    ,sum(case when source='tw' then 1 else 0 end) as tw_count
-    ,sum(case when source='fb' then 1 else 0 end) as fb_count
-    ,sum(case when source='sw' then 1 else 0 end) as sw_count
+    ,sum(case when source='home' then 1 else 0 end) as home_count
+    ,sum(case when source='ts' then 1 else 0 end) as ts_count
+    ,sum(case when source='fb_share' then 1 else 0 end) as fb_share_count
     ,sum(case when source='ks' then 1 else 0 end) as ks_count
-    ,sum(case when source='kt' then 1 else 0 end) as kt_count
-    ,sum(case when source='blog' then 1 else 0 end) as blog_count
-    ,sum(case when source='fbapp' then 1 else 0 end) as fbapp_count
-    ,sum(case when source='page' then 1 else 0 end) as page_count
-    ,sum(case when source='lm' then 1 else 0 end) as mobileapp_count
-    ,sum(case when source='edm' then 1 else 0 end) as edm_count
-    ,sum(case when source not in ('lgh','sum37','lc','tw','fb','sw','ks','kt','blog','fbapp','page','lm','edm') then 1 else 0 end) as etc_count
+    ,sum(case when source not in ('home', 'ts', 'fb_share' ,'ks') then 1 else 0 end) as etc_count
     ,count(*) as total_count").group("date(convert_tz(created_at,'+00:00','+09:00'))").order("date(convert_tz(created_at,'+00:00','+09:00'))")
     @traffic_stats_sum = TrafficLog.select("
-     sum(case when source='lgh' then 1 else 0 end) as lgh_count
-    ,sum(case when source='sum37' then 1 else 0 end) as sum37_count
-    ,sum(case when source='lc' then 1 else 0 end) as lc_count
-    ,sum(case when source='tw' then 1 else 0 end) as tw_count
-    ,sum(case when source='fb' then 1 else 0 end) as fb_count
-    ,sum(case when source='sw' then 1 else 0 end) as sw_count
+     sum(case when source='home' then 1 else 0 end) as home_count
+    ,sum(case when source='ts' then 1 else 0 end) as ts_count
+    ,sum(case when source='fb_share' then 1 else 0 end) as fb_share_count
     ,sum(case when source='ks' then 1 else 0 end) as ks_count
-    ,sum(case when source='kt' then 1 else 0 end) as kt_count
-    ,sum(case when source='blog' then 1 else 0 end) as blog_count
-    ,sum(case when source='fbapp' then 1 else 0 end) as fbapp_count
-    ,sum(case when source='page' then 1 else 0 end) as page_count
-    ,sum(case when source='lm' then 1 else 0 end) as mobileapp_count
-    ,sum(case when source='edm' then 1 else 0 end) as edm_count
-    ,sum(case when source not in ('lgh','sum37','lc','tw','fb','sw','ks','kt','blog','fbapp','page','lm','edm') then 1 else 0 end) as etc_count
+    ,sum(case when source not in ('home', 'ts', 'fb_share' ,'ks') then 1 else 0 end) as etc_count
     ,count(*) as total_count")
   end
   
