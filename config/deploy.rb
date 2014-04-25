c86d298520fe8d0937f788ee533007a52986ed0d
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-04-25 11:44:20 +0900

    comments에 profile picture, facebook_url 추가

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :user, "deployer"
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml config/secrets.yml log/production.log log/development.log}
-
+set :linked_dirs, fetch(:linked_dirs) + %w{public/system public/uploads}
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
